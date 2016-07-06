022380a2ef5c34820946a447493e8401a4c386fc
Sora Lee <sora_lsh@163.com>
2016-07-06 09:59:11 +0800

    Add test case for class Goods

---
+++ src/org/qpt/receipt/Goods.java
@@ -0,0 +1,7 @@
+package org.qpt.receipt;
+
+/**
+ * Created by Sora on 2016/7/5.
+ */
+public class Goods {
+}
---
+++ test/org/qpt/receipt/GoodsTest.java
@@ -0,0 +1,34 @@
+package org.qpt.receipt;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sora on 2016/7/5.
+ */
+public class GoodsTest {
+
+    private static final String BARCODE     = "ITEM000001";
+    private static final String NAME        = "可口可乐";
+    private static final String UNIT        = "瓶";
+    private static final String CATEGORY    = "食品";
+    private static final String SUBCATEGORY = "碳酸饮料";
+    private static final float  PRICE        = 3.00f;
+
+    @Test
+    public void shouldStoreGoodsInfoOK() throws Exception {
+        Goods goods = new Goods(BARCODE, NAME);
+        goods.setCategory(CATEGORY, SUBCATEGORY);
+        goods.setPrice(PRICE, UNIT);
+
+        assertNotNull(goods);
+
+        assertEquals(BARCODE,       goods.getBarcode());
+        assertEquals(NAME,          goods.getName());
+        assertEquals(CATEGORY,      goods.getCategory());
+        assertEquals(SUBCATEGORY,   goods.getSubCategory());
+        assertEquals(UNIT,          goods.getUnit());
+        assertEquals(PRICE,         goods.getPrice());
+    }
+}
